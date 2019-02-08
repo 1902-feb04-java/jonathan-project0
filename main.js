a13f9af672c0c242bbca97b3f8351dc54de4cc2e
jburtson <jburtson@gmail.com>
2019-02-08 11:02:27 -0600

    Added hidden element, functions for changing speed

--- main.js
+++ main.js
@@ -1,27 +1,75 @@
 "using strict";
 
+// global variables
+let intervalSpeed = 1000;
 let gold = 0;
+let isRevealed = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-    let text1 = document.getElementById("text1");
-    let button1 = document.getElementById("button1");
-    text1.textContent = ":O";
-    let loadTime = Date.now();
-    let millis = Date.now()-loadTime;
-    let timePassed = Math.floor(millis/1000);
-    text1.textContent = `${timePassed}`;
+    // element references
+    let goldElement = document.getElementById("goldCount");
+    let mineButton = document.getElementById("mine");
+    let clearButton = document.getElementById("clear");
+    let hidden = document.getElementById("hid");
 
-    button1.addEventListener("click", () => {
+    // time eclapsed
+    // let loadTime = Date.now();
+    // let millis = Date.now()-loadTime;
+    // let timePassed = Math.floor(millis/1000);
+    // goldElement.textContent = `${timePassed}`;
+
+    // call update() every second (1000 milliseconds)
+    var intervalID = window.setInterval(update, intervalSpeed);
+    // to stop this interval:
+    // clearInterval(intervalID);
+
+    // on click of button
+    mineButton.addEventListener("click", () => {
         gold++;
-        updateGold();
+        refreshGold();
     });
-
-    var intervalID = window.setInterval(myCallback, 1000);
-    function myCallback() {
+    clearButton.addEventListener("click", () => {
+        gold=0;
+        refreshGold();
+    });
+    
+    // runs every second
+    function update() {
         gold++;
-        updateGold();
+        refreshGold();
+
+        if (!isRevealed && gold >= 10){
+            isRevealed = true;
+            hidden.style.display = "block";
+        }
     }
-    function updateGold(){
-        text1.textContent = `${gold}`;
+
+    // update gold count
+    function refreshGold(){
+        if (gold==0){
+            goldElement.textContent = `Gold: *nothing*`;
+        }
+        
+        goldElement.textContent = `Gold: ${gold}`;
+    }
+
+    // set interval speed
+    function setInterval(speed){
+        if (speed > 0){
+            clearInterval(intervalID);
+            intervalSpeed = speed;
+            intervalID = window.setInterval(update, intervalSpeed);
+        }
+    }
+    // change interval speed
+    function modifyInterval(speed){
+        if ((intervalSpeed + speed) > 0){
+            clearInterval(intervalID);
+            intervalSpeed += speed;
+            intervalID = window.setInterval(update, intervalSpeed);
+        }
+        else{
+            console.log("Too fast! Can't modify interval.");
+        }
     }
 });
