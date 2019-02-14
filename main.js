5e25b075933d6c574c1855c495f8bef86dcb4fec
jburtson <jburtson@gmail.com>
2019-02-14 14:40:33 -0600

    Added pet FSM with a linked animation, and a energy level

--- main.js
+++ main.js
@@ -7,15 +7,53 @@ let money = 0;
 let hid1Revealed = false;
 let hid2Revealed = false;
 
-// class asciiAnimation (strArray){
+// pet stuff
+let petEnergy = 0;
+let maxPetEnergy = 100;
+let petState = 0;
+let petStates = ["Sleep", "Eat", "Idle"];
 
-// }
+function changePetState(newState){
+    if (newState >= 0 && newState<petStates.length){
+        petState = newState;
+    }
+    return petStates[petState];
+}
 
+// ES6 class
+class AnimLoop {
+    constructor(anim){
+        this.frame = 0;
+        this.animation = anim;
+        this.frameSize = this.animation.length;
+    }
+    next(){
+        this.frame = (this.frame + 1) % this.frameSize;
+        return this.animation[this.frame];
+    }
+    current(){
+        return this.animation[this.frame];
+    }
+}
+// ES5 object creation
+function makeAnimation(anim){
+    return {
+        frame: 0,
+        animation: anim,
+        frameSize: anim.length,
+        next: function(){
+            this.frame = (this.frame + 1) % this.frameSize;
+            return this.animation[this.frame];
+        }
+    }
+}
+
+// ON LOAD ---------------------------------------------------------
 document.addEventListener("DOMContentLoaded", () => {
     // element references
     let goldElement = document.getElementById("goldCount");
     let mineButton = document.getElementById("mine");
-
+    
     // hidden elements
     let hiddenElements = document.getElementsByClassName("startHidden");
     let hid1 = document.getElementById("hid1");
@@ -23,10 +61,16 @@ document.addEventListener("DOMContentLoaded", () => {
     let moneyElement = document.getElementById("money");
     let hid2 = document.getElementById("hid2");
     let pet = document.getElementById("pet");
-    let sleepListAlt = ["(-_-) zZz","(-.-) ZzZ"];
-    let sleepList = ["(˘o˘ ) zZz","(˘O˘ ) ZzZ"];
-    let sleepState = 0;
+    let petStateElement = document.getElementById("petState");
 
+    // pet animations
+    let sleepAnim = new AnimLoop(["(˘o˘ ) zZz","(˘O˘ ) ZzZ"]); // ES6 class
+    let eatAnim = new AnimLoop(["(・—・)⊃`","(・Θ・)つ "]);
+    let idleAnim = new AnimLoop(["(・_・)","(・_・)","(・_・)","(ᨆ_ᨆ)"]);
+    let animationState = [sleepAnim,eatAnim,idleAnim];
+    // \xao space character
+    //let sleepAnim = makeAnimation(["(˘o˘ ) zZz","(˘O˘ ) ZzZ"]); // ES5 object
+    
     // time eclapsed
     // let loadTime = Date.now();
     // let millis = Date.now()-loadTime;
@@ -44,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
         refreshGold();
     });
     
-    // runs every second
+    // runs every second ------------------------------------------------------------------------
     function update() {
         gold++;
         refreshGold();
@@ -62,16 +106,32 @@ document.addEventListener("DOMContentLoaded", () => {
                 refreshMoney();
             });
         }
-        // runs once, revealing hid2
+        // runs once, revealing hid2, PET
         if (!hid2Revealed && money >= 20){
             hid2Revealed = true;
             // reveal all hidden elements
             hid2.style.display = "inline";
+            petStateElement.textContent = changePetState(0);
         }
+        // update pet
         if (hid2Revealed){
             // PET
-            sleepState = (sleepState + 1) % sleepList.length;
-            pet.textContent = sleepList[sleepState];
+            switch(petState){
+                case 0: // sleeping
+                if (petEnergy < maxPetEnergy){
+                    petEnergy+=10;
+                }
+                else{
+                    petStateElement.textContent = changePetState(2);
+                }
+                break;
+                case 1: // eating
+                break;
+                case 2: // idle
+                break;
+            }
+            // NEXT FRAME OF ANIMATION
+            pet.textContent = animationState[petState].next();
         }
     }
 
